24b8ae881d790a400d6b9b616bd5c4a83351e3da
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2018-08-15 18:08:19 -0500

    fix for js and css load issues
    css and js files were not loading
    I placed them in a public folder and added express.static("public") to app.js
    voila!

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require("express"),
     app     = express();
     
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.render("colorGame");
 });
 
-app.get("*", function(req, res){
-	res.redirect("/");
-});
+// app.get("*", function(req, res){
+// 	res.redirect("/");
+// });
 
 app.listen(process.env.PORT, process.env.IP);
