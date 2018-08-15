185ce9b543c78a995ab78cb95c58cd67aa43ab92
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2018-08-15 13:57:45 -0500

    restructured
    still figuring out sublime vs ide

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require("express"),
 // app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("colorGame.html");
+    res.render("colorGame");
 });
 
 app.get("*", function(req, res){
