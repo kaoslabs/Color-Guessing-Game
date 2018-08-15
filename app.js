75f7ff1e39faee0e45fa6bccd1a30214fb1fcbd3
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2018-08-15 13:37:54 -0500

    error fixes
    fixed html pages loaded

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var express = require("express"),
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("home");
+    res.render("colorGame");
 });
 
-app.get("/about", function(req, res){
-    res.render("about");
+app.get("*", function(req, res){
+	res.redirect("/");
 });
 
 app.listen(process.env.PORT, process.env.IP);
