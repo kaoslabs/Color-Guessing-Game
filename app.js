44da2d7ae333e91a9babd4d011f96a2ab1c18af7
kaoslabs <42324929+kaoslabs@users.noreply.github.com>
2018-08-15 14:00:44 -0500

    switched to ejs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express"),
     app     = express();
     
-// app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("colorGame");
